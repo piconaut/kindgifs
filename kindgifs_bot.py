d23dbbb5c82d68163d2639794afdb48630a4e73d
ianfare <apellius@gmail.com>
2017-06-04 01:21:26 -0400

    Added bot

---
+++ kindgifs_bot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import tweepy, time, subprocess
+
+#Twitter credentials
+CONSUMER_KEY = 'X'
+CONSUMER_SECRET = 'X'
+ACCESS_KEY = 'X'
+ACCESS_SECRET = 'X'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+while True:
+  print('Getting most recent 10 mentions')
+  mentions = api.mentions_timeline(count=10)
+  print('Got mentions! (^-^)')
+  with open('./replies.txt','r') as f:
+    replies = f.readlines()
+  for i in range(len(replies)):
+    replies[i] = replies[i].strip()
+  for mention in mentions:
+    if str(mention.id) not in replies:
+      replies.append(str(mention.id))
+      print(mention.text)
+      text = ' '.join(mention.text.split('@kindgifs ')[1:])
+      try:
+        subprocess.call(['./kindgifs/kindgifs.py', text])
+        fname = text.replace(' ','_') + '.gif'
+        print('@'+mention.user.screen_name)
+        api.update_with_media(fname,status='@'+mention.user.screen_name,in_reply_to_status_id=mention.id)
+        print('Reply successful! (^-^)')
+        subprocess.call(['rm',fname])
+        print('Cleaned up gif!')
+      except:
+        pass
+  with open('replies.txt','w') as f:
+    for reply in replies:
+      f.write(reply + '\n')
+  time.sleep(15) #Sleep for 15 seconds
