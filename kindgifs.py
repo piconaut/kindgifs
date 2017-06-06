9a74d853d5fab2db7a41f03b9adb4bc0c368b9a4
ianfare <apellius@gmail.com>
2017-06-06 21:09:07 +0000

    special characters

--- kindgifs.py
+++ kindgifs.py
@@ -6,20 +6,20 @@ import imageio
 import os
 
 height = 10
-bgcolour = (255,117,255)
-txtcolour = (255,255,255)
+bg = (255,117,255)
+txt = (255,255,255)
 
 y0 = 2
 
-row0 = {'a':[txtcolour,txtcolour,txtcolour],'b':[txtcolour,txtcolour,txtcolour],'c':[bgcolour,txtcolour,txtcolour],'d':[txtcolour,txtcolour,bgcolour],'e':[txtcolour,txtcolour,txtcolour],'f':[txtcolour,txtcolour,txtcolour],'g':[bgcolour,txtcolour,txtcolour],'h':[txtcolour,bgcolour,txtcolour],'i':[txtcolour,txtcolour,txtcolour],'j':[txtcolour,txtcolour,txtcolour],'k':[txtcolour,bgcolour,txtcolour],'l':[txtcolour,bgcolour,bgcolour],'m':[txtcolour,txtcolour,txtcolour],'n':[txtcolour,txtcolour,bgcolour],'o':[bgcolour,txtcolour,txtcolour],'p':[txtcolour,txtcolour,txtcolour],'q':[bgcolour,txtcolour,bgcolour],'r':[txtcolour,txtcolour,txtcolour],'s':[bgcolour,txtcolour,txtcolour],'t':[txtcolour,txtcolour,txtcolour],'u':[txtcolour,bgcolour,txtcolour],'v':[txtcolour,bgcolour,txtcolour],'w':[txtcolour,bgcolour,txtcolour],'x':[txtcolour,bgcolour,txtcolour],'y':[txtcolour,bgcolour,txtcolour],'z':[txtcolour,txtcolour,txtcolour],' ':[bgcolour,bgcolour,bgcolour]}
+row0 = {'a':[txt,txt,txt],'b':[txt,txt,txt],'c':[bg,txt,txt],'d':[txt,txt,bg],'e':[txt,txt,txt],'f':[txt,txt,txt],'g':[bg,txt,txt],'h':[txt,bg,txt],'i':[txt,txt,txt],'j':[txt,txt,txt],'k':[txt,bg,txt],'l':[txt,bg,bg],'m':[txt,txt,txt],'n':[txt,txt,bg],'o':[bg,txt,txt],'p':[txt,txt,txt],'q':[bg,txt,bg],'r':[txt,txt,txt],'s':[bg,txt,txt],'t':[txt,txt,txt],'u':[txt,bg,txt],'v':[txt,bg,txt],'w':[txt,bg,txt],'x':[txt,bg,txt],'y':[txt,bg,txt],'z':[txt,txt,txt],' ':[bg,bg,bg],'*':[txt,bg,txt],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[bg,txt,bg],'~':[bg,bg,bg]}
 
-row1 = {'a':[txtcolour,bgcolour,txtcolour],'b':[txtcolour,bgcolour,txtcolour],'c':[txtcolour,bgcolour,bgcolour],'d':[txtcolour,bgcolour,txtcolour],'e':[txtcolour,bgcolour,bgcolour],'f':[txtcolour,bgcolour,bgcolour],'g':[txtcolour,bgcolour,bgcolour],'h':[txtcolour,bgcolour,txtcolour],'i':[bgcolour,txtcolour,bgcolour],'j':[bgcolour,txtcolour,bgcolour],'k':[txtcolour,bgcolour,txtcolour],'l':[txtcolour,bgcolour,bgcolour],'m':[txtcolour,txtcolour,txtcolour],'n':[txtcolour,bgcolour,txtcolour],'o':[txtcolour,bgcolour,txtcolour],'p':[txtcolour,bgcolour,txtcolour],'q':[txtcolour,bgcolour,txtcolour],'r':[txtcolour,bgcolour,txtcolour],'s':[txtcolour,bgcolour,bgcolour],'t':[bgcolour,txtcolour,bgcolour],'u':[txtcolour,bgcolour,txtcolour],'v':[txtcolour,bgcolour,txtcolour],'w':[txtcolour,bgcolour,txtcolour],'x':[txtcolour,bgcolour,txtcolour],'y':[txtcolour,bgcolour,txtcolour],'z':[bgcolour,bgcolour,txtcolour],' ':[bgcolour,bgcolour,bgcolour]}
+row1 = {'a':[txt,bg,txt],'b':[txt,bg,txt],'c':[txt,bg,bg],'d':[txt,bg,txt],'e':[txt,bg,bg],'f':[txt,bg,bg],'g':[txt,bg,bg],'h':[txt,bg,txt],'i':[bg,txt,bg],'j':[bg,txt,bg],'k':[txt,bg,txt],'l':[txt,bg,bg],'m':[txt,txt,txt],'n':[txt,bg,txt],'o':[txt,bg,txt],'p':[txt,bg,txt],'q':[txt,bg,txt],'r':[txt,bg,txt],'s':[txt,bg,bg],'t':[bg,txt,bg],'u':[txt,bg,txt],'v':[txt,bg,txt],'w':[txt,bg,txt],'x':[txt,bg,txt],'y':[txt,bg,txt],'z':[bg,bg,txt],' ':[bg,bg,bg],'*':[bg,txt,bg],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[txt,bg,txt],'~':[bg,bg,txt]}
 
-row2 = {'a':[txtcolour,txtcolour,txtcolour],'b':[txtcolour,txtcolour,bgcolour],'c':[txtcolour,bgcolour,bgcolour],'d':[txtcolour,bgcolour,txtcolour],'e':[txtcolour,txtcolour,bgcolour],'f':[txtcolour,txtcolour,bgcolour],'g':[txtcolour,bgcolour,bgcolour],'h':[txtcolour,txtcolour,txtcolour],'i':[bgcolour,txtcolour,bgcolour],'j':[bgcolour,txtcolour,bgcolour],'k':[txtcolour,txtcolour,bgcolour],'l':[txtcolour,bgcolour,bgcolour],'m':[txtcolour,bgcolour,txtcolour],'n':[txtcolour,bgcolour,txtcolour],'o':[txtcolour,bgcolour,txtcolour],'p':[txtcolour,txtcolour,txtcolour],'q':[txtcolour,bgcolour,txtcolour],'r':[txtcolour,txtcolour,bgcolour],'s':[txtcolour,txtcolour,txtcolour],'t':[bgcolour,txtcolour,bgcolour],'u':[txtcolour,bgcolour,txtcolour],'v':[txtcolour,bgcolour,txtcolour],'w':[txtcolour,bgcolour,txtcolour],'x':[bgcolour,txtcolour,bgcolour],'y':[txtcolour,txtcolour,txtcolour],'z':[bgcolour,txtcolour,bgcolour],' ':[bgcolour,bgcolour,bgcolour]}
+row2 = {'a':[txt,txt,txt],'b':[txt,txt,bg],'c':[txt,bg,bg],'d':[txt,bg,txt],'e':[txt,txt,bg],'f':[txt,txt,bg],'g':[txt,bg,bg],'h':[txt,txt,txt],'i':[bg,txt,bg],'j':[bg,txt,bg],'k':[txt,txt,bg],'l':[txt,bg,bg],'m':[txt,bg,txt],'n':[txt,bg,txt],'o':[txt,bg,txt],'p':[txt,txt,txt],'q':[txt,bg,txt],'r':[txt,txt,bg],'s':[txt,txt,txt],'t':[bg,txt,bg],'u':[txt,bg,txt],'v':[txt,bg,txt],'w':[txt,bg,txt],'x':[bg,txt,bg],'y':[txt,txt,txt],'z':[bg,txt,bg],' ':[bg,bg,bg],'*':[txt,txt,txt],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[txt,bg,txt],'~':[txt,txt,txt]}
 
-row3 = {'a':[txtcolour,bgcolour,txtcolour],'b':[txtcolour,bgcolour,txtcolour],'c':[txtcolour,bgcolour,bgcolour],'d':[txtcolour,bgcolour,txtcolour],'e':[txtcolour,bgcolour,bgcolour],'f':[txtcolour,bgcolour,bgcolour],'g':[txtcolour,bgcolour,txtcolour],'h':[txtcolour,bgcolour,txtcolour],'i':[bgcolour,txtcolour,bgcolour],'j':[bgcolour,txtcolour,bgcolour],'k':[txtcolour,bgcolour,txtcolour],'l':[txtcolour,bgcolour,bgcolour],'m':[txtcolour,bgcolour,txtcolour],'n':[txtcolour,bgcolour,txtcolour],'o':[txtcolour,bgcolour,txtcolour],'p':[txtcolour,bgcolour,bgcolour],'q':[txtcolour,txtcolour,bgcolour],'r':[txtcolour,bgcolour,txtcolour],'s':[bgcolour,bgcolour,txtcolour],'t':[bgcolour,txtcolour,bgcolour],'u':[txtcolour,bgcolour,txtcolour],'v':[txtcolour,txtcolour,txtcolour],'w':[txtcolour,txtcolour,txtcolour],'x':[txtcolour,bgcolour,txtcolour],'y':[bgcolour,bgcolour,txtcolour],'z':[txtcolour,bgcolour,bgcolour],' ':[bgcolour,bgcolour,bgcolour]}
+row3 = {'a':[txt,bg,txt],'b':[txt,bg,txt],'c':[txt,bg,bg],'d':[txt,bg,txt],'e':[txt,bg,bg],'f':[txt,bg,bg],'g':[txt,bg,txt],'h':[txt,bg,txt],'i':[bg,txt,bg],'j':[bg,txt,bg],'k':[txt,bg,txt],'l':[txt,bg,bg],'m':[txt,bg,txt],'n':[txt,bg,txt],'o':[txt,bg,txt],'p':[txt,bg,bg],'q':[txt,txt,bg],'r':[txt,bg,txt],'s':[bg,bg,txt],'t':[bg,txt,bg],'u':[txt,bg,txt],'v':[txt,txt,txt],'w':[txt,txt,txt],'x':[txt,bg,txt],'y':[bg,bg,txt],'z':[txt,bg,bg],' ':[bg,bg,bg],'*':[bg,txt,bg],'.':[bg,bg,bg],',':[bg,bg,txt],'@':[txt,bg,bg],'~':[txt,bg,bg]}
 
-row4 = {'a':[txtcolour,bgcolour,txtcolour],'b':[txtcolour,txtcolour,txtcolour],'c':[bgcolour,txtcolour,txtcolour],'d':[txtcolour,txtcolour,txtcolour],'e':[txtcolour,txtcolour,txtcolour],'f':[txtcolour,bgcolour,bgcolour],'g':[txtcolour,txtcolour,txtcolour],'h':[txtcolour,bgcolour,txtcolour],'i':[txtcolour,txtcolour,txtcolour],'j':[txtcolour,txtcolour,bgcolour],'k':[txtcolour,bgcolour,txtcolour],'l':[txtcolour,txtcolour,txtcolour],'m':[txtcolour,bgcolour,txtcolour],'n':[txtcolour,bgcolour,txtcolour],'o':[txtcolour,txtcolour,bgcolour],'p':[txtcolour,bgcolour,bgcolour],'q':[bgcolour,txtcolour,txtcolour],'r':[txtcolour,bgcolour,txtcolour],'s':[txtcolour,txtcolour,bgcolour],'t':[bgcolour,txtcolour,bgcolour],'u':[bgcolour,txtcolour,txtcolour],'v':[bgcolour,txtcolour,bgcolour],'w':[txtcolour,txtcolour,txtcolour],'x':[txtcolour,bgcolour,txtcolour],'y':[txtcolour,txtcolour,txtcolour],'z':[txtcolour,txtcolour,txtcolour],' ':[bgcolour,bgcolour,bgcolour]}
+row4 = {'a':[txt,bg,txt],'b':[txt,txt,txt],'c':[bg,txt,txt],'d':[txt,txt,txt],'e':[txt,txt,txt],'f':[txt,bg,bg],'g':[txt,txt,txt],'h':[txt,bg,txt],'i':[txt,txt,txt],'j':[txt,txt,bg],'k':[txt,bg,txt],'l':[txt,txt,txt],'m':[txt,bg,txt],'n':[txt,bg,txt],'o':[txt,txt,bg],'p':[txt,bg,bg],'q':[bg,txt,txt],'r':[txt,bg,txt],'s':[txt,txt,bg],'t':[bg,txt,bg],'u':[bg,txt,txt],'v':[bg,txt,bg],'w':[txt,txt,txt],'x':[txt,bg,txt],'y':[txt,txt,txt],'z':[txt,txt,txt],' ':[bg,bg,bg],'*':[txt,bg,txt],'.':[bg,txt,bg],',':[bg,txt,bg],'@':[bg,txt,txt],'~':[bg,bg,bg]}
 
 if len(sys.argv) > 1:
   text = ' '.join(sys.argv[1:])
@@ -33,37 +33,37 @@ pixels = []
 for i in range(height):
   pixels.append([])
   for j in range(5):
-    pixels[i].append(bgcolour)
+    pixels[i].append(bg)
 
 for char in text:
   for i in range(3):
-    pixels[0].append(bgcolour)
-    pixels[1].append(bgcolour)
+    pixels[0].append(bg)
+    pixels[1].append(bg)
     pixels[y0].append(row0[char][i])
     pixels[y0+1].append(row1[char][i])
     pixels[y0+2].append(row2[char][i])
     pixels[y0+3].append(row3[char][i])
     pixels[y0+4].append(row4[char][i])
-    pixels[7].append(bgcolour)
-    pixels[8].append(bgcolour)
-    pixels[9].append(bgcolour)
+    pixels[7].append(bg)
+    pixels[8].append(bg)
+    pixels[9].append(bg)
   for i in range(10):
-    pixels[i].append(bgcolour)
+    pixels[i].append(bg)
 
 for i in range(height):
   pixels.append([])
   for j in range(4):
-    pixels[i].append(bgcolour)
+    pixels[i].append(bg)
 
-pixels[4][2] = txtcolour
-pixels[5][1] = txtcolour
-pixels[5][3] = txtcolour
-pixels[6][2] = txtcolour
+pixels[4][2] = txt
+pixels[5][1] = txt
+pixels[5][3] = txt
+pixels[6][2] = txt
 
-pixels[4][-3] = txtcolour
-pixels[5][-2] = txtcolour
-pixels[5][-4] = txtcolour
-pixels[6][-3] = txtcolour
+pixels[4][-3] = txt
+pixels[5][-2] = txt
+pixels[5][-4] = txt
+pixels[6][-3] = txt
 
 width = len(pixels[0])
 
@@ -91,37 +91,37 @@ pixels = []
 for i in range(height):
   pixels.append([])
   for j in range(5):
-    pixels[i].append(bgcolour)
+    pixels[i].append(bg)
 
 for char in text:
   for i in range(3):
-    pixels[0].append(bgcolour)
-    pixels[1].append(bgcolour)
-    pixels[2].append(bgcolour)
+    pixels[0].append(bg)
+    pixels[1].append(bg)
+    pixels[2].append(bg)
     pixels[y0].append(row0[char][i])
     pixels[y0+1].append(row1[char][i])
     pixels[y0+2].append(row2[char][i])
     pixels[y0+3].append(row3[char][i])
     pixels[y0+4].append(row4[char][i])
-    pixels[8].append(bgcolour)
-    pixels[9].append(bgcolour)
+    pixels[8].append(bg)
+    pixels[9].append(bg)
   for i in range(10):
-    pixels[i].append(bgcolour)
+    pixels[i].append(bg)
 
 for i in range(height):
   pixels.append([])
   for j in range(4):
-    pixels[i].append(bgcolour)
+    pixels[i].append(bg)
 
-pixels[3][2] = txtcolour
-pixels[4][1] = txtcolour
-pixels[4][3] = txtcolour
-pixels[5][2] = txtcolour
+pixels[3][2] = txt
+pixels[4][1] = txt
+pixels[4][3] = txt
+pixels[5][2] = txt
 
-pixels[3][-3] = txtcolour
-pixels[4][-2] = txtcolour
-pixels[4][-4] = txtcolour
-pixels[5][-3] = txtcolour
+pixels[3][-3] = txt
+pixels[4][-2] = txt
+pixels[4][-4] = txt
+pixels[5][-3] = txt
 
 width = len(pixels[0])
 
