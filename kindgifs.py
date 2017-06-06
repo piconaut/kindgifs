d0a1bad1c482405bef65a6ac90f3b499a95da76b
ianfare <apellius@gmail.com>
2017-06-06 23:04:34 +0000

    Added numbers

--- kindgifs.py
+++ kindgifs.py
@@ -7,19 +7,19 @@ import os
 
 height = 10
 bg = (255,117,255)
-txt = (255,255,255)
+tx = (255,255,255)
 
 y0 = 2
 
-row0 = {'a':[txt,txt,txt],'b':[txt,txt,txt],'c':[bg,txt,txt],'d':[txt,txt,bg],'e':[txt,txt,txt],'f':[txt,txt,txt],'g':[bg,txt,txt],'h':[txt,bg,txt],'i':[txt,txt,txt],'j':[txt,txt,txt],'k':[txt,bg,txt],'l':[txt,bg,bg],'m':[txt,txt,txt],'n':[txt,txt,bg],'o':[bg,txt,txt],'p':[txt,txt,txt],'q':[bg,txt,bg],'r':[txt,txt,txt],'s':[bg,txt,txt],'t':[txt,txt,txt],'u':[txt,bg,txt],'v':[txt,bg,txt],'w':[txt,bg,txt],'x':[txt,bg,txt],'y':[txt,bg,txt],'z':[txt,txt,txt],' ':[bg,bg,bg],'*':[txt,bg,txt],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[bg,txt,bg],'~':[bg,bg,bg],'!':[bg,txt,bg],'?':[txt,txt,txt],"\'":[bg,bg,txt],'\"':[txt,bg,txt],'#':[txt,bg,txt]}
+row0 = {'a':[tx,tx,tx],'b':[tx,tx,tx],'c':[bg,tx,tx],'d':[tx,tx,bg],'e':[tx,tx,tx],'f':[tx,tx,tx],'g':[bg,tx,tx],'h':[tx,bg,tx],'i':[tx,tx,tx],'j':[tx,tx,tx],'k':[tx,bg,tx],'l':[tx,bg,bg],'m':[tx,tx,tx],'n':[tx,tx,bg],'o':[bg,tx,tx],'p':[tx,tx,tx],'q':[bg,tx,bg],'r':[tx,tx,tx],'s':[bg,tx,tx],'t':[tx,tx,tx],'u':[tx,bg,tx],'v':[tx,bg,tx],'w':[tx,bg,tx],'x':[tx,bg,tx],'y':[tx,bg,tx],'z':[tx,tx,tx],' ':[bg,bg,bg],'*':[tx,bg,tx],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[bg,tx,bg],'~':[bg,bg,bg],'!':[bg,tx,bg],'?':[tx,tx,tx],"\'":[bg,bg,tx],'\"':[tx,bg,tx],'#':[tx,bg,tx],'_':[bg,bg,bg],'0':[tx,tx,tx],'1':[tx,tx,bg],'2':[tx,tx,tx],'3':[tx,tx,tx],'4':[tx,bg,tx],'5':[tx,tx,tx],'6':[tx,bg,bg],'7':[tx,tx,tx],'8':[tx,tx,tx],'9':[tx,tx,tx]}
 
-row1 = {'a':[txt,bg,txt],'b':[txt,bg,txt],'c':[txt,bg,bg],'d':[txt,bg,txt],'e':[txt,bg,bg],'f':[txt,bg,bg],'g':[txt,bg,bg],'h':[txt,bg,txt],'i':[bg,txt,bg],'j':[bg,txt,bg],'k':[txt,bg,txt],'l':[txt,bg,bg],'m':[txt,txt,txt],'n':[txt,bg,txt],'o':[txt,bg,txt],'p':[txt,bg,txt],'q':[txt,bg,txt],'r':[txt,bg,txt],'s':[txt,bg,bg],'t':[bg,txt,bg],'u':[txt,bg,txt],'v':[txt,bg,txt],'w':[txt,bg,txt],'x':[txt,bg,txt],'y':[txt,bg,txt],'z':[bg,bg,txt],' ':[bg,bg,bg],'*':[bg,txt,bg],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[txt,bg,txt],'~':[bg,bg,txt],'!':[bg,txt,bg],'?':[bg,bg,txt],"\'":[bg,txt,bg],'\"':[txt,bg,txt],'#':[txt,txt,txt]}
+row1 = {'a':[tx,bg,tx],'b':[tx,bg,tx],'c':[tx,bg,bg],'d':[tx,bg,tx],'e':[tx,bg,bg],'f':[tx,bg,bg],'g':[tx,bg,bg],'h':[tx,bg,tx],'i':[bg,tx,bg],'j':[bg,tx,bg],'k':[tx,bg,tx],'l':[tx,bg,bg],'m':[tx,tx,tx],'n':[tx,bg,tx],'o':[tx,bg,tx],'p':[tx,bg,tx],'q':[tx,bg,tx],'r':[tx,bg,tx],'s':[tx,bg,bg],'t':[bg,tx,bg],'u':[tx,bg,tx],'v':[tx,bg,tx],'w':[tx,bg,tx],'x':[tx,bg,tx],'y':[tx,bg,tx],'z':[bg,bg,tx],' ':[bg,bg,bg],'*':[bg,tx,bg],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[tx,bg,tx],'~':[bg,bg,tx],'!':[bg,tx,bg],'?':[bg,bg,tx],"\'":[bg,tx,bg],'\"':[tx,bg,tx],'#':[tx,tx,tx],'_':[bg,bg,bg],'0':[tx,bg,tx],'1':[bg,tx,bg],'2':[bg,bg,tx],'3':[bg,bg,tx],'4':[tx,bg,tx],'5':[tx,bg,bg],'6':[tx,bg,bg],'7':[bg,bg,tx],'8':[tx,bg,tx],'9':[tx,bg,tx]}
 
-row2 = {'a':[txt,txt,txt],'b':[txt,txt,bg],'c':[txt,bg,bg],'d':[txt,bg,txt],'e':[txt,txt,bg],'f':[txt,txt,bg],'g':[txt,bg,bg],'h':[txt,txt,txt],'i':[bg,txt,bg],'j':[bg,txt,bg],'k':[txt,txt,bg],'l':[txt,bg,bg],'m':[txt,bg,txt],'n':[txt,bg,txt],'o':[txt,bg,txt],'p':[txt,txt,txt],'q':[txt,bg,txt],'r':[txt,txt,bg],'s':[txt,txt,txt],'t':[bg,txt,bg],'u':[txt,bg,txt],'v':[txt,bg,txt],'w':[txt,bg,txt],'x':[bg,txt,bg],'y':[txt,txt,txt],'z':[bg,txt,bg],' ':[bg,bg,bg],'*':[txt,txt,txt],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[txt,bg,txt],'~':[txt,txt,txt],'!':[bg,txt,bg],'?':[bg,txt,txt],"\'":[bg,bg,bg],'\"':[bg,bg,bg],'#':[txt,bg,txt]}
+row2 = {'a':[tx,tx,tx],'b':[tx,tx,bg],'c':[tx,bg,bg],'d':[tx,bg,tx],'e':[tx,tx,bg],'f':[tx,tx,bg],'g':[tx,bg,bg],'h':[tx,tx,tx],'i':[bg,tx,bg],'j':[bg,tx,bg],'k':[tx,tx,bg],'l':[tx,bg,bg],'m':[tx,bg,tx],'n':[tx,bg,tx],'o':[tx,bg,tx],'p':[tx,tx,tx],'q':[tx,bg,tx],'r':[tx,tx,bg],'s':[tx,tx,tx],'t':[bg,tx,bg],'u':[tx,bg,tx],'v':[tx,bg,tx],'w':[tx,bg,tx],'x':[bg,tx,bg],'y':[tx,tx,tx],'z':[bg,tx,bg],' ':[bg,bg,bg],'*':[tx,tx,tx],'.':[bg,bg,bg],',':[bg,bg,bg],'@':[tx,bg,tx],'~':[tx,tx,tx],'!':[bg,tx,bg],'?':[bg,tx,tx],"\'":[bg,bg,bg],'\"':[bg,bg,bg],'#':[tx,bg,tx],'_':[bg,bg,bg],'0':[tx,bg,tx],'1':[bg,tx,bg],'2':[tx,tx,tx],'3':[bg,tx,tx],'4':[tx,tx,tx],'5':[tx,tx,tx],'6':[tx,tx,tx],'7':[bg,bg,tx],'8':[tx,tx,tx],'9':[tx,tx,tx]}
 
-row3 = {'a':[txt,bg,txt],'b':[txt,bg,txt],'c':[txt,bg,bg],'d':[txt,bg,txt],'e':[txt,bg,bg],'f':[txt,bg,bg],'g':[txt,bg,txt],'h':[txt,bg,txt],'i':[bg,txt,bg],'j':[bg,txt,bg],'k':[txt,bg,txt],'l':[txt,bg,bg],'m':[txt,bg,txt],'n':[txt,bg,txt],'o':[txt,bg,txt],'p':[txt,bg,bg],'q':[txt,txt,bg],'r':[txt,bg,txt],'s':[bg,bg,txt],'t':[bg,txt,bg],'u':[txt,bg,txt],'v':[txt,txt,txt],'w':[txt,txt,txt],'x':[txt,bg,txt],'y':[bg,bg,txt],'z':[txt,bg,bg],' ':[bg,bg,bg],'*':[bg,txt,bg],'.':[bg,bg,bg],',':[bg,bg,txt],'@':[txt,bg,bg],'~':[txt,bg,bg],'!':[bg,bg,bg],'?':[bg,bg,bg],"\'":[bg,bg,bg],'\"':[bg,bg,bg],'#':[txt,txt,txt]}
+row3 = {'a':[tx,bg,tx],'b':[tx,bg,tx],'c':[tx,bg,bg],'d':[tx,bg,tx],'e':[tx,bg,bg],'f':[tx,bg,bg],'g':[tx,bg,tx],'h':[tx,bg,tx],'i':[bg,tx,bg],'j':[bg,tx,bg],'k':[tx,bg,tx],'l':[tx,bg,bg],'m':[tx,bg,tx],'n':[tx,bg,tx],'o':[tx,bg,tx],'p':[tx,bg,bg],'q':[tx,tx,bg],'r':[tx,bg,tx],'s':[bg,bg,tx],'t':[bg,tx,bg],'u':[tx,bg,tx],'v':[tx,tx,tx],'w':[tx,tx,tx],'x':[tx,bg,tx],'y':[bg,bg,tx],'z':[tx,bg,bg],' ':[bg,bg,bg],'*':[bg,tx,bg],'.':[bg,bg,bg],',':[bg,bg,tx],'@':[tx,bg,bg],'~':[tx,bg,bg],'!':[bg,bg,bg],'?':[bg,bg,bg],"\'":[bg,bg,bg],'\"':[bg,bg,bg],'#':[tx,tx,tx],'_':[bg,bg,bg],'0':[tx,bg,tx],'1':[bg,tx,bg],'2':[tx,bg,bg],'3':[bg,bg,tx],'4':[bg,bg,tx],'5':[bg,bg,tx],'6':[tx,bg,tx],'7':[bg,bg,tx],'8':[tx,bg,tx],'9':[bg,bg,tx]}
 
-row4 = {'a':[txt,bg,txt],'b':[txt,txt,txt],'c':[bg,txt,txt],'d':[txt,txt,txt],'e':[txt,txt,txt],'f':[txt,bg,bg],'g':[txt,txt,txt],'h':[txt,bg,txt],'i':[txt,txt,txt],'j':[txt,txt,bg],'k':[txt,bg,txt],'l':[txt,txt,txt],'m':[txt,bg,txt],'n':[txt,bg,txt],'o':[txt,txt,bg],'p':[txt,bg,bg],'q':[bg,txt,txt],'r':[txt,bg,txt],'s':[txt,txt,bg],'t':[bg,txt,bg],'u':[bg,txt,txt],'v':[bg,txt,bg],'w':[txt,txt,txt],'x':[txt,bg,txt],'y':[txt,txt,txt],'z':[txt,txt,txt],' ':[bg,bg,bg],'*':[txt,bg,txt],'.':[bg,txt,bg],',':[bg,txt,bg],'@':[bg,txt,txt],'~':[bg,bg,bg],'!':[bg,txt,bg],'?':[bg,txt,bg],"\'":[bg,bg,bg],'\"':[bg,bg,bg],'#':[txt,bg,txt]}
+row4 = {'a':[tx,bg,tx],'b':[tx,tx,tx],'c':[bg,tx,tx],'d':[tx,tx,tx],'e':[tx,tx,tx],'f':[tx,bg,bg],'g':[tx,tx,tx],'h':[tx,bg,tx],'i':[tx,tx,tx],'j':[tx,tx,bg],'k':[tx,bg,tx],'l':[tx,tx,tx],'m':[tx,bg,tx],'n':[tx,bg,tx],'o':[tx,tx,bg],'p':[tx,bg,bg],'q':[bg,tx,tx],'r':[tx,bg,tx],'s':[tx,tx,bg],'t':[bg,tx,bg],'u':[bg,tx,tx],'v':[bg,tx,bg],'w':[tx,tx,tx],'x':[tx,bg,tx],'y':[tx,tx,tx],'z':[tx,tx,tx],' ':[bg,bg,bg],'*':[tx,bg,tx],'.':[bg,tx,bg],',':[bg,tx,bg],'@':[bg,tx,tx],'~':[bg,bg,bg],'!':[bg,tx,bg],'?':[bg,tx,bg],"\'":[bg,bg,bg],'\"':[bg,bg,bg],'#':[tx,bg,tx],'_':[tx,tx,tx],'0':[tx,tx,tx],'1':[tx,tx,tx],'2':[tx,tx,tx],'3':[tx,tx,tx],'4':[bg,bg,tx],'5':[tx,tx,tx],'6':[tx,tx,tx],'7':[bg,bg,tx],'8':[tx,tx,tx],'9':[bg,bg,tx]}
 
 if len(sys.argv) > 1:
   text = ' '.join(sys.argv[1:])
@@ -56,15 +56,15 @@ for i in range(height):
   for j in range(4):
     pixels[i].append(bg)
 
-pixels[4][2] = txt
-pixels[5][1] = txt
-pixels[5][3] = txt
-pixels[6][2] = txt
+pixels[4][2] = tx
+pixels[5][1] = tx
+pixels[5][3] = tx
+pixels[6][2] = tx
 
-pixels[4][-3] = txt
-pixels[5][-2] = txt
-pixels[5][-4] = txt
-pixels[6][-3] = txt
+pixels[4][-3] = tx
+pixels[5][-2] = tx
+pixels[5][-4] = tx
+pixels[6][-3] = tx
 
 width = len(pixels[0])
 
@@ -114,15 +114,15 @@ for i in range(height):
   for j in range(4):
     pixels[i].append(bg)
 
-pixels[3][2] = txt
-pixels[4][1] = txt
-pixels[4][3] = txt
-pixels[5][2] = txt
+pixels[3][2] = tx
+pixels[4][1] = tx
+pixels[4][3] = tx
+pixels[5][2] = tx
 
-pixels[3][-3] = txt
-pixels[4][-2] = txt
-pixels[4][-4] = txt
-pixels[5][-3] = txt
+pixels[3][-3] = tx
+pixels[4][-2] = tx
+pixels[4][-4] = tx
+pixels[5][-3] = tx
 
 width = len(pixels[0])
 
